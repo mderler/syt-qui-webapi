dda0599554b670b4ad48260e3a5dd466273ac6f0
mderler <5matthiasderler5@gmail.com>
2023-12-19 23:35:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import CpuVariable from './components/CpuVariable.vue';
+
+const variables = ["ein"]
+</script>
+
+<template>
+  <h1>Variables</h1>
+  <CpuVariable v-for="cpuVar in variables" :key="cpuVar" :name="cpuVar" />
+</template>
