53e2d20a5ad3dca3a24fdef77badb28d60472a71
MatthiasDerler <5matthiasderler5@gmail.com>
2023-12-20 10:53:49 +0100

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import CpuVariable from './components/CpuVariable.vue';
+import CpuVariable from '@/components/CpuVariable.vue';
+import type { VarType } from '@/types/var-type.type'
 
-const variables = ["ein"]
+const variables: [{ name: string, type: VarType }] = [{ name: "ein", type: "bool" }]
 </script>
 
 <template>
   <h1>Variables</h1>
-  <CpuVariable v-for="cpuVar in variables" :key="cpuVar" :name="cpuVar" />
+  <CpuVariable v-for="cpuVar in variables" :key="cpuVar.name" :name="cpuVar.name" :var-type="cpuVar.type" />
 </template>
--- src/components/CpuVariable.vue
+++ src/components/CpuVariable.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
-defineProps<{ name: string }>()
+import { ref } from 'vue'
+import type { VarType } from '@/types/var-type.type'
+
+defineProps<{ name: string, varType: VarType }>()
+
+const varValue = ref<boolean | number>()
+
+const save = () => {
+  fetch("https://192.168.10.62/api/jsonrpc", {body: JSON.stringify({})})
+}
 </script>
 
 <template>
   <div>
-    <span>{{ name }}</span>
+    <span class="right-distance">{{ name }}</span>
+    <span class="right-distance">
+      <span>Value:</span>
+      <select v-if="varType === 'bool'" v-model="varValue">
+        <option :value="false">False</option>
+        <option :value="true">True</option>
+      </select>
+      <input type="number" v-else v-model="varValue">
+    </span>
+    <button @click="save">Save</button>
   </div>
 </template>
+
+<style scoped>
+.right-distance {
+  margin-right: 1rem;
+}
+</style>
